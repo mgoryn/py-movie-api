b4867c72faf2cc8dace2bccf47fce33f7cde0f2d
Misha Goryn <mgoryn68@gmail.com>
2024-11-29 23:08:34 +0200

    Add blank lines for: serializers.py urls.py views.py

--- cinema/views.py
+++ cinema/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
+
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
