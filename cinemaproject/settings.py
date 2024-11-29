f7ee5575d02bb6209affcd49c89eb801e6ab410e
Misha Goryn <mgoryn68@gmail.com>
2024-11-29 21:51:07 +0200

    Update description on models.py & settings.py > change Debug > False, add SECRET_KEY to .env file

--- cinemaproject/settings.py
+++ cinemaproject/settings.py
@@ -19,11 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-d^*_y2j%#014-x)%gq&^0hoglyafo941f&61f9hbs7k2fu%5#2"
-
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
@@ -105,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "Europe/Kiev"
 
 TIME_ZONE = "UTC"
 
